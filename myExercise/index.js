fcc4a4c9fcf3908ca46d79a228ed1407be25b821
zuobaiquan <zuobaiquan@qq.com>
2018-12-02 18:21:36 +0800

    dd

--- myExercise/index.js
+++ myExercise/index.js
@@ -6,21 +6,13 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
+  AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class myExercise extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -28,10 +20,7 @@ export default class App extends Component<Props> {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+          1122
         </Text>
       </View>
     );
@@ -56,3 +45,5 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+AppRegistry.registerComponent('myExercise', () => myExercise);
